8eaca6269427591cc27206c7708dbe8093e88185
HimanshuK18 <himanshukhurana18@gmail.com>
2023-02-23 15:39:01 +1100

    custom hook

--- src/projects/Children.tsx
+++ src/projects/Children.tsx
@@ -1,19 +1,20 @@
 import ImageSlider from "./ImageSlider";
+import { usePasswordToggler } from '../hook/usePasswordToggler';
 
 export default function Children(props: any) {
+    const { type, passwordVisibility, handlePasswordVisibility } = usePasswordToggler();
     return (<>
-<ImageSlider name="hello">
-        <div />
-
-        <div>this to show the children</div>
-
-        <div>{false}</div>
-
-        <div>{null}</div>
-
-        <div>{undefined}</div>
-
-        <div>{true}</div>
+        <ImageSlider name="hello">
+            <div />
+            <div>this to show the children</div>
+            <div>{false}</div>
+            <div>{null}</div>
+            <div>{undefined}</div>
+            <div>{true}</div>
         </ImageSlider>
+        <div>
+            <input type={type} placeholder='Enter password...' />
+            <button onClick={handlePasswordVisibility}>{passwordVisibility ? 'Show' : 'Hide'} Password</button>
+        </div>
     </>);
 }
