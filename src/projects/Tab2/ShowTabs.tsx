90527c836ab1298b09550bcbb3d8aa41a5c1e843
HimanshuK18 <himanshukhurana18@gmail.com>
2023-02-28 13:24:55 +1100

    tab code

--- src/projects/Tab2/ShowTabs.tsx
+++ src/projects/Tab2/ShowTabs.tsx
@@ -2,9 +2,9 @@ import TabControl from './TabControl';
 
 function AppTab2() {
   const tabs = [
-    { label: 'Tab 1', content: <div>Tab 1 content</div> },
-    { label: 'Tab 2', content: <div>Tab 2 content</div> },
-    { label: 'Tab 3', content: <div>Tab 3 content</div> },
+    { label: 'Tab 1', content: <div>Tab 1 content</div>, contentPath: '../Clock.tsx' },
+    { label: 'Tab 2', content: <div>Tab 2 content</div>, contentPath: '../Tab/ComponentB.tsx'  },
+    { label: 'Tab 3', content: <div>Tab 3 content</div>, contentPath: '../Tab/ComponentA.tsx'  },
   ];
 
   return (
---
+++ src/projects/Tab2/TabControl.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import './Tab.css';
+
+function TabControl(props) {
+  const [activeTab, setActiveTab] = useState(0);
+  const [Component, setComponent] = useState(null);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+  
+
+  useEffect(() => {
+    const path = '../Clock.tsx';
+    const loadComponent = async (index) => {
+      const componentModule = await import(`../Clock.tsx`);
+      setComponent(() => componentModule.default);
+    };
+    loadComponent(activeTab);
+
+  }, [activeTab,props.tabs]);
+
+  return (
+    <div className="TabContent">
+      <div className="TabContent">
+        {props.tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={index === activeTab ? 'active' : ''}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="tab-content">
+      {Component && <Component />}
+      </div>
+    </div>
+  );
+}
+export default TabControl;
